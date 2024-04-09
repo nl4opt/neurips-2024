cb5e9f4c455c60e0199bfd69ec58f40ac0955616
nl4opt <nl4opt@gmail.com>
2024-04-09 15:50:17 -0700

    Fixed an issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nl4opt.github.io',
-  base: '/neurips-2024',
+  base: '/neurips-2024/',
   jsx: 'react',
   integrations: [
     react(),
